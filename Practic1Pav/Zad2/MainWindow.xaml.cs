238e6050ae50db15d0b8bca749d1e3799a82d712
Stiv0r <Sriv0r@icloud.com>
2023-05-23 18:08:13 +0300

    Сделал 1 практику 3 задания

---
+++ Practic1Pav/Zad2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace Practic1Pavishin
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(vvod.Text);
+            double x1 = 1.0;
+            double y1 = 2.0;
+            double x2 = 4.0;
+            double y2 = 6.0;
+
+            double distance = Sqrt(Pow(x2 - x1, 2) + Pow(y2 - y1, 2));
+
+            otvet.Text = ("Расстояние между точками: " + distance);
+        }
+    }
+}
---
+++ Practic1Pav/Zad3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practic1Pavishin
+{
+    /// <summary>
+    /// Логика взаимодействия для Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double a = double.Parse(X.Text);
+            double b = double.Parse(A.Text);
+            double c = double.Parse(Y.Text);
+
+            double pricePerKg = a / b;
+
+            double totalPrice = pricePerKg * c;
+
+            Answer.Text = ("Стоимость 1 кг конфет: " + pricePerKg);
+            Answer_Copy.Text = ("Стоимость " + c + " кг конфет: " + totalPrice);
+
+        }
+    }
+}
+
+
